6ae95b170c1ad539295610e1de90b5f72ebb0503
Mohit Kumar <emkayatiitr@gmail.com>
2019-06-01 17:17:45 +0530

    Fixed stackblitz loading issue

--- src/app/shared-popups/comp1/comp1.component.ts
+++ src/app/shared-popups/comp1/comp1.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, ViewChild, TemplateRef } from '@angular/core';
-import { PopupService } from 'src/app/popup/popup.service';
+import { PopupService } from '../../popup/popup.service';
 
 @Component({
   selector: 'app-comp1',
